b021b78bbd839a635db1605e94ae69477ba915ea
HrebtovAlexey <31938743+HrebtovAlexey@users.noreply.github.com>
2017-09-29 21:19:11 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+    var val = expression.split('*');
+    var zeros = 0;
+    var startNum = 0;
+    var inc = 0;
+    var DF = true;
+  
+    for(var i = 0; i < val.length; i++) {
+    	if(val[i].match(/!!/)) {
+            val[i] = parseInt(val[i].match(/\d{1,}/));
+            	if(val[i] % 2 === 0) { startNum = 2;}
+            	else {startNum = 1;}
+            	inc = 2;
+          	}else {
+				val[i] = parseInt(val[i].match(/\d{1,}/));
+            	startNum = 1;
+            	inc = 1;
+         	}
+         if(startNum === 2 || inc == 1) {
+         	DF = false;
+         }
+         for(startNum; startNum <= val[i]; startNum += inc) {
+             if(startNum % 5 === 0 ) zeros++;
+             if(startNum % 25 === 0) zeros++;
+          }
+      }
+	if(DF) {
+		return 0
+	}else {
+		return zeros
+	}
+};
